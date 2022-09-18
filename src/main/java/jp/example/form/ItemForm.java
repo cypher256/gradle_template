9a2c9e424d81e0b8e378d1148cea907adab602de
cypher256 <kashihara@me.com>
2022-09-18 14:52:07 +0900

    更新

--- src/main/java/jp/example/form/ItemForm.java
+++ src/main/java/jp/example/form/ItemForm.java
@@ -64,6 +64,7 @@ public class ItemForm {
 	 * 入力値を検証します。 <br>
 	 * 不正な場合はアプリエラーを表す IllegalStateException をスローします。 
 	 * @param req HTTP サーブレットリクエスト (エラー表示用に、このフォームをリクエスト属性にセット)
+	 * @return このインスタンス
 	 */
 	public ItemForm validate(HttpServletRequest req) {
 		req.setAttribute("form", this);
